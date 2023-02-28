d8f91484f5a0d853bb003c5badb63d720ffcdb49
S0mePonch <Dialga98765@gmail.com>
2023-02-27 20:18:00 -0600

    Changes to a mini-game

---
+++ Intro612/ViewController3.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController3.swift
+//  Intro612
+//
+//  Created by ISSC_612_2023 on 27/02/23.
+//
+/*
+ import UIKit
+ 
+ class ViewController3: UIViewController {
+ 
+ @IBOutlet weak var op1: UITextField!
+ @IBOutlet weak var op2: UITextField!
+ @IBOutlet weak var btnOp: UIButton!
+ @IBOutlet weak var Exit: UILabel!
+ 
+ func calcular(_ a:Double, _ b:Double) -> Double{
+ return a + b
+ }
+ 
+ @IBAction func btnClick(_ sender: UIButton) {
+ 
+ Exit.text = String(calcular(Double(op1.text!)!, Double(op2.text!)!))
+ }
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ 
+ // Do any additional setup after loading the view.
+ }
+ 
+ 
+ }
+ */
---
+++ ViewController4.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController4.swift
+//  Intro612
+//
+//  Created by ISSC_612_2023 on 27/02/23.
+//
+
+import UIKit
+
+class ViewController4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
